89ad153698906e3744e4cee44816283bfeaf09a0
Satoshi Warita <div9851.tech@gmail.com>
2021-09-01 22:57:53 +0900

    add dotenv,cors,helmet

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
+import * as dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import router from "./routes/v1/index";
+
+dotenv.config();
 
 const app = express();
 
-// JSONオブジェクトの受信設定
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
-// 配列側のオブジェクトの受信設定
 app.use(express.urlencoded({ extended: true }));
 
 // ルーティング
 app.use("/v1", router);
 
-// 3000ポートで受信
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // APIサーバ起動
 app.listen(port);
