533cc3b23af586a09deb7a2e7e92ccfca75005b1
Satoshi Warita <div9851.tech@gmail.com>
2021-09-01 23:24:42 +0900

    add auth

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,29 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
+
 import router from "./routes/v1/index";
 
 dotenv.config();
+
+const jwksUri = process.env.JWKS_URI || "";
+const audience = process.env.AUDIENCE || "";
+const issuer = process.env.ISSUER || "";
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: jwksUri,
+  }),
+
+  audience: audience,
+  issuer: [issuer],
+  algorithms: ["RS256"],
+});
 
 const app = express();
 
@@ -12,6 +32,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(checkJwt);
 
 // ルーティング
 app.use("/v1", router);
