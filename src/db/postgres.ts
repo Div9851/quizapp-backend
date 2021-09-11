0f766f941134166f4ba1a3c92147d96268b9ea37
Satoshi Warita <div9851.tech@gmail.com>
2021-09-11 10:56:50 +0900

    enable ssl

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -5,6 +5,9 @@ const pool: Pool = new Pool({
   connectionString: env.databaseUrl,
   max: 20,
   idleTimeoutMillis: 30000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
